2719c8f0f4b174248ff55fe5fba86b2924f371a5
Christopher Batin <chris@batin.me>
2021-02-13 19:08:48 +0000

    Added snapshot test

--- test/get_stat_dashboard.test.ts
+++ test/get_stat_dashboard.test.ts
@@ -1,13 +1,14 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as GetStatDashboard from '../lib/get_stat_dashboard-stack';
+
+const regions = { IRELAND: 'eu-west-1'}
+const env = { account: '07609764445', region: regions.IRELAND }
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack');
+    const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
