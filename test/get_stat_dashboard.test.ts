25bf29b3d88adcaf3d5facc8d55102a37e759372
Christopher Batin <chris@batin.me>
2021-02-14 11:01:37 +0000

    Fixed unit tests

--- test/get_stat_dashboard.test.ts
+++ test/get_stat_dashboard.test.ts
@@ -1,14 +1,130 @@
-import { SynthUtils } from '@aws-cdk/assert';
+import '@aws-cdk/assert/jest';
 import * as cdk from '@aws-cdk/core';
 import * as GetStatDashboard from '../lib/get_stat_dashboard-stack';
 
 const regions = { IRELAND: 'eu-west-1'}
 const env = { account: '076097644456', region: regions.IRELAND }
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
-    // THEN
-    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
-});
+describe('All Projects handler', () => {
+    test('Lambda handler', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('AWS::Lambda::Function', {
+            Handler: "allProjects.main",
+            Runtime: "nodejs12.x"
+        });
+    });
+
+    test('Log retention handler', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('Custom::LogRetention', {
+            RetentionInDays: 14
+        });
+    });
+
+    test('IAM Role', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('AWS::IAM::Role', {});
+    });
+})
+
+describe('All Sites handler', () => {
+    test('Lambda handler', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('AWS::Lambda::Function', {
+            Handler: "allSites.main",
+            Runtime: "nodejs12.x"
+        });
+    });
+
+    test('Log retention handler', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('Custom::LogRetention', {
+            RetentionInDays: 14
+        });
+    });
+
+    test('IAM Role', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('AWS::IAM::Role', {});
+    });
+})
+
+describe('Tags handler', () => {
+    test('Lambda handler', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('AWS::Lambda::Function', {
+            Handler: "tags.main",
+            Runtime: "nodejs12.x"
+        });
+    });
+
+    test('Log retention handler', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('Custom::LogRetention', {
+            RetentionInDays: 14
+        });
+    });
+
+    test('IAM Role', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('AWS::IAM::Role', {});
+    });
+})
+
+describe('Main handler', () => {
+    test('Lambda handler', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('AWS::Lambda::Function', {
+            Handler: "index.main",
+            Runtime: "nodejs12.x"
+        });
+    });
+
+    test('Log retention handler', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('Custom::LogRetention', {
+            RetentionInDays: 14
+        });
+    });
+
+    test('IAM Role', () => {
+        const app = new cdk.App();
+        // WHEN
+        const stack = new GetStatDashboard.GetStatDashboardStack(app, 'MyTestStack', { env: env });
+        // THEN
+        expect(stack).toHaveResource('AWS::IAM::Role', {});
+    });
+})
