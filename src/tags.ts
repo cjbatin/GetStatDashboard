0f74a0ec717898d15dd63ddc0f10c159a19d7d5d
Christopher Batin <chris@batin.me>
2021-02-17 21:46:59 +0000

    Added pagination to the tags page

--- src/tags.ts
+++ src/tags.ts
@@ -4,10 +4,18 @@ import fetch from "node-fetch";
 import * as ejs from 'ejs';
 import * as fs from 'fs';
 
+const resultsPerPage = 100;
+
 export const main: APIGatewayProxyHandlerV2 = async (event) => {
   if (event.pathParameters !== undefined && event.pathParameters.apiKey !== undefined 
     && event.pathParameters.siteId !== undefined) {
-    return tags(event.pathParameters.apiKey, event.pathParameters.siteId) 
+      if (event.queryStringParameters !== undefined && event.queryStringParameters.page !== undefined){
+        console.log("Page number")
+        console.log(event.queryStringParameters.pageNumber)
+        return tags(event.pathParameters.apiKey, event.pathParameters.siteId, Number(event.queryStringParameters.page)) 
+      } else {
+        return tags(event.pathParameters.apiKey, event.pathParameters.siteId, 1) 
+      }
   } else {
     return {
       "statusCode": 404,
@@ -17,18 +25,27 @@ export const main: APIGatewayProxyHandlerV2 = async (event) => {
   }
 };
 
-async function tags(apiKey: string, siteId: string) {
+async function tags(apiKey: string, siteId: string, pageNumber: number) {
   try {
+    const resultsPerPageString = resultsPerPage.toString()
       const apiResponse = await fetch(
-          "https://journeyfurther.getstat.com/api/v2/" + apiKey + "/tags/list?format=json&site_id=" + siteId
+          "https://journeyfurther.getstat.com/api/v2/" + apiKey + "/tags/list?format=json&results="+ resultsPerPageString + "&site_id=" + siteId + "&start=" + startSearchAt(pageNumber)
       )
       const apiResponseJson = await apiResponse.json()
       const htmlContent = fs.readFileSync(__dirname + '/views/tags.ejs', 'utf8');
+      console.log("Response")
+      console.log(apiResponseJson)
+      console.log("Number of pages")
+      const pages = numberOfPages(apiResponseJson.Response.totalresults)
+      console.log(pages)
       const htmlRenderized = ejs.render(htmlContent, {
         filename: 'views/tags.ejs', 
         data: {
           apiKey,
-          results: convertResultToArray(apiResponseJson.Response.Result)
+          results: convertResultToArray(apiResponseJson.Response.Result),
+          numberOfPages: pages,
+          pageNumber: pageNumber,
+          siteId: siteId
         }
       });
       return {
@@ -44,4 +61,14 @@ async function tags(apiKey: string, siteId: string) {
         "headers": {"Content-Type": "text/html"}
     }
   }
+}
+
+function numberOfPages(totalResultsString: string): Number {
+  const totalResults = Number(totalResultsString)
+  return Math.ceil(totalResults / resultsPerPage)
+}
+
+function startSearchAt(pageNumber: number): string {
+  const searchAtNumber = (pageNumber - 1) * resultsPerPage
+  return searchAtNumber.toString()
 }
